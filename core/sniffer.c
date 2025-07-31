7fd497fafc9732c5714afd836bf697fca92469d6
By-Ress <ortancabaris67@gmail.com>
2025-07-31 13:40:26 +0300

    new project sniffer

---
+++ core/sniffer.c
@@ -0,0 +1,3 @@
+//
+// Created by Baris Ortanca on 31/07/2025.
+//
---
+++ include/sniffer.h
@@ -0,0 +1,8 @@
+//
+// Created by Baris Ortanca on 31/07/2025.
+//
+
+#ifndef SNIFFER_PROJECT_SNIFFER_H
+#define SNIFFER_PROJECT_SNIFFER_H
+
+#endif //SNIFFER_PROJECT_SNIFFER_H
